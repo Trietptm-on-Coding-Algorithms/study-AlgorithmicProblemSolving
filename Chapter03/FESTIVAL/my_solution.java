33be26e4a9ee7bc5636fbf865592309fa2051fe3
Yegun Kim <ibocon@github.com>
2016-01-14 21:00:19 -0600

    CLOCKSYNC

--- Chapter03/FESTIVAL/my_solution.java
+++ Chapter03/FESTIVAL/my_solution.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class my_solution {
   public static void main(String[] args){
     Scanner sc = new Scanner(System.in);
     int cases = sc.nextInt();
